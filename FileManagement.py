234122acd19c9122d7e882fe9c0ae876a7c5ae6b
Jolin Qiu <qiujolin1@gmail.com>
2023-08-08 19:53:34 -0400

    currently only tracks changes using watchdog in the current directory : 2023-08-08 19:52:21 - Created file: /Users/jolinqiu/CSCIprojects/CSCIhomework/Finder-Organizer/cs_queue.py 2023-08-08 19:52:21 - Modified directory: /Users/jolinqiu/CSCIprojects/CSCIhomework/Finder-Organizer 2023-08-08 19:52:28 - Deleted file: /Users/jolinqiu/CSCIprojects/CSCIhomework/Finder-Organizer/cs_queue.py 2023-08-08 19:52:28 - Modified directory: /Users/jolinqiu/CSCIprojects/CSCIhomework/Finder-Organizer 2023-08-08 19:52:45 - Created file: /Users/jolinqiu/CSCIprojects/CSCIhomework/Finder-Organizer/.DS_Store 2023-08-08 19:52:45 - Modified directory: /Users/jolinqiu/CSCIprojects/CSCIhomework/Finder-Organizer 2023-08-08 19:52:45 - Modified file: /Users/jolinqiu/CSCIprojects/CSCIhomework/Finder-Organizer/.DS_Store

--- FileManagement.py
+++ FileManagement.py
@@ -1,16 +1,55 @@
 
 import os
+import time
+from dataclasses import dataclass
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+import sys
+import logging
+from watchdog.events import LoggingEventHandler
 
-def main():
+filePath = "/Users/jolinqiu/Downloads"
+
+def getSumn():
     """
 
     :return:
     """
-    try:
-        print("yo")
-    except:
-        print("exception")
+    # os.scandir() returns a Python iterable containing the names of the files
+    # and subdirectories in the directory given by the path argument:
+    # items = os.scandir(filePath)
+
+    with os.scandir(filePath) as items:
+        for item in items:
+            print(item.name)
 
 
+# event handler
 if __name__ == "__main__":
-    main()
+    """
+    The event handler is the object that will be notified when something happen on the filesystem you are monitoring.
+    """
+    # Set the format for logging info
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    # Set format for displaying path
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+
+    # Initialize logging event handler
+    event_handler = LoggingEventHandler()
+
+    # Initialize Observer
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+
+    # Start the observer
+    observer.start()
+    try:
+        while True:
+            # Set the thread sleep time
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
